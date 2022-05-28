c7bef9fd726573d2a3a62ef08b984f1b8b8125d8
Niko Barateli <nbara15@freeuni.edu.ge>
2022-05-28 18:12:55 +0400

    sequential coroutine

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,13 +1,29 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
-
+        uiState.postValue(UiState.Loading)
+        viewModelScope.launch {
+            try {
+                uiState.postValue(
+                    UiState.Success(
+                        mockApi.getAndroidVersionFeatures(
+                            mockApi.getRecentAndroidVersions().last().apiLevel
+                        )
+                    )
+                )
+            } catch (e: Exception) {
+                uiState.postValue(UiState.Error(e.message ?: "Request failed!"))
+            }
+        }
     }
+
 }
