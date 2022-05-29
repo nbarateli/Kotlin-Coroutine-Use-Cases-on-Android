c047223ad94415a39236bc83e9021f7bcb6e42c3
Niko Barateli <nbara15@freeuni.edu.ge>
2022-05-29 17:41:33 +0400

    await async

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -3,6 +3,8 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -29,6 +31,23 @@ class PerformNetworkRequestsConcurrentlyViewModel(
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.postLoading()
+
+        viewModelScope.launch {
+            try {
+
+                uiState.postSuccess(
+                    awaitAll(
+                        viewModelScope.async { mockApi.getAndroidVersionFeatures(27) },
+                        viewModelScope.async { mockApi.getAndroidVersionFeatures(28) },
+                        viewModelScope.async { mockApi.getAndroidVersionFeatures(29) },
+                    )
+                )
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.postError(e)
+            }
+        }
 
     }
 }
