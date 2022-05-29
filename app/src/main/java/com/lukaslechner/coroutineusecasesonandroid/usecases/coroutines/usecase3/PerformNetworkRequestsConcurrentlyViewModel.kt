28e9bf2c634a5a183af932c48a1b86c9fe0aa93a
Niko Barateli <nbara15@freeuni.edu.ge>
2022-05-29 16:12:07 +0400

    3 sequential

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -1,14 +1,31 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class PerformNetworkRequestsConcurrentlyViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
-
+        uiState.postLoading()
+        viewModelScope.launch {
+            try {
+                uiState.postSuccess(
+                    listOf(
+                        mockApi.getAndroidVersionFeatures(27),
+                        mockApi.getAndroidVersionFeatures(28),
+                        mockApi.getAndroidVersionFeatures(29),
+                    )
+                )
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.postError(e)
+            }
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
