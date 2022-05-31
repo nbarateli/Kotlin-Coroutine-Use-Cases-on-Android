4c331ae622064fe491d1f499dd3108d654d2f60e
nbarateli <nbara15@freeuni.edu.ge>
2022-05-31 12:36:31 +0400

    high order retry with expo backoff end

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/base/Util.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/base/Util.kt
@@ -1,23 +1,25 @@
 package com.lukaslechner.coroutineusecasesonandroid.base
 
+import kotlinx.coroutines.delay
 import timber.log.Timber
+import kotlin.math.min
 
 suspend fun <T> retry(
     retries: Int,
     initialDelayMillis: Long = 100L,
     maxDelayMillis: Long = 1000L,
-    factor: Double = 2.0,
     onRepeatError: (t: Throwable) -> Any = { Timber.e(it) },
     block: suspend () -> T
 ): T {
 
 
+    var pow = 0
     repeat(retries) {
-
-    try {
+        try {
             return block()
         } catch (e: Exception) {
             onRepeatError(e)
+            delay(min(initialDelayMillis shl ++pow, maxDelayMillis))
         }
     }
 
